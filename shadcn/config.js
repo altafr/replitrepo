33f96d23cbedefbbbd1cebe80f2c1ef27d95ec95
altafr <altafr@gmail.com>
2024-09-07 15:40:16 +0000

    Initial commit

---
+++ shadcn/config.js
@@ -0,0 +1,13 @@
+// shadcn.config.js
+module.exports = {
+  theme: {
+    colors: {
+      primary: '#1DA1F2',
+      secondary: '#14171A',
+      accent: '#657786',
+      // Add more custom colors if needed
+    },
+  },
+  variants: {},
+  plugins: [],
+}
